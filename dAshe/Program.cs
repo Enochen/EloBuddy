7e8b7ddc40aec5b63cb6118c9c9ce55582a5c836
Darakath <darakath1128@gmail.com>
2015-09-19 07:25:53 -0400

    Removed Waveclear

--- dAshe/Program.cs
+++ dAshe/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Drawing;
+using SharpDX;
 using System.Collections.Generic;
 using EloBuddy;
 using EloBuddy.SDK;
@@ -10,30 +10,29 @@ using EloBuddy.SDK.Enumerations;
 
 namespace dAshe
 {
-    using SharpDX;
+    using System.Linq;
 
     class Program
 	{
 
 		private static Menu mainMenu;
-        private static Slider manaQ, manaW, manaR;
-		private static Dictionary<SpellSlot, CheckBox> comboSpellStatus = new Dictionary<SpellSlot, CheckBox>();
-		private static Dictionary<SpellSlot, CheckBox> waveClearSpellStatus = new Dictionary<SpellSlot, CheckBox>();
-        public static Dictionary<SpellSlot, Spell.SpellBase> spells = new Dictionary<SpellSlot, Spell.SpellBase>()
+        private static Slider manaQ, manaW, manaR, manaFarm, manaHarass;
+		private static Dictionary<SpellSlot, CheckBox> comboSS = new Dictionary<SpellSlot, CheckBox>();
+		private static Dictionary<SpellSlot, CheckBox> waveClearSS = new Dictionary<SpellSlot, CheckBox>();
+        private static Dictionary<SpellSlot, CheckBox> harassSS = new Dictionary<SpellSlot, CheckBox>();
+        public static Dictionary<SpellSlot, Spell.SpellBase> Spells = new Dictionary<SpellSlot, Spell.SpellBase>()
         {
             {SpellSlot.Q, new Spell.Active(SpellSlot.Q)},
             {SpellSlot.W, new Spell.Skillshot(SpellSlot.W, 1200, SkillShotType.Linear, 250, 1500, 20)},
             {SpellSlot.R, new Spell.Skillshot(SpellSlot.R, 25000, SkillShotType.Linear, 250, 1600, 130)}
         };
-        public static object W = spells[SpellSlot.W];
-        public static object R = spells[SpellSlot.R];
 
         static void Main(string[] args)
 		{
-			Loading.OnLoadingComplete += onLoadComplete;
+			Loading.OnLoadingComplete += OnLoadComplete;
 		}
 
-		private static void onLoadComplete(EventArgs args)
+		private static void OnLoadComplete(EventArgs args)
 		{
 			if (Player.Instance.ChampionName != "Ashe")
 				return;
@@ -42,16 +41,22 @@ namespace dAshe
 			Orbwalker.OnPreAttack += Orbwalker_OnPreAttack;
             mainMenu = MainMenu.AddMenu("dAshe", "dAsheMenu");
             mainMenu.AddGroupLabel("dAshe by Darakath - Enjoy the bugs");
+            mainMenu.AddSeparator();
             mainMenu.AddGroupLabel("Combo");
-			comboSpellStatus[SpellSlot.Q] = mainMenu.Add("comboUseQ", new CheckBox("Use Q"));
-			comboSpellStatus[SpellSlot.W] = mainMenu.Add("comboUseW", new CheckBox("Use W"));
-			comboSpellStatus[SpellSlot.R] = mainMenu.Add("comboUseR", new CheckBox("Use R"));
-			mainMenu.AddGroupLabel("Farming");
-		    waveClearSpellStatus[SpellSlot.W] = mainMenu.Add("waveClearUseW", new CheckBox("Use W"));
+			comboSS[SpellSlot.Q] = mainMenu.Add("comboUseQ", new CheckBox("Use Q"));
+			comboSS[SpellSlot.W] = mainMenu.Add("comboUseW", new CheckBox("Use W"));
+			comboSS[SpellSlot.R] = mainMenu.Add("comboUseR", new CheckBox("Use R"));
+            mainMenu.AddSeparator();
+			//mainMenu.AddGroupLabel("Waveclear");
+		    //waveClearSS[SpellSlot.W] = mainMenu.Add("waveClearUseW", new CheckBox("Use W"));
+            mainMenu.AddGroupLabel("Harass");
+            harassSS[SpellSlot.W] = mainMenu.Add("harassUseW", new CheckBox("Use W"));
+            mainMenu.AddSeparator();
             mainMenu.AddGroupLabel("Mana Options");
 		    manaQ = mainMenu.Add("manaQ", new Slider("Min Mana % to Q"));
             manaW = mainMenu.Add("manaW", new Slider("Min Mana % to W"));
-            manaR = mainMenu.Add("manaR", new Slider("Min Mana % to R"));
+		    manaR = mainMenu.Add("manaR", new Slider("Min Mana % to R"));
+            manaHarass = mainMenu.Add("manaHarass", new Slider("Min Mana % to Harass"));
             Game.OnTick += Game_OnTick;
             Chat.Print("dAshe By Darakath loaded.", Color.DarkBlue);
 		}
@@ -66,40 +71,57 @@ namespace dAshe
 				case Orbwalker.ActiveModes.LaneClear:
 					WaveClear();
 					break;
+                case Orbwalker.ActiveModes.Harass:
+			        Harass();
+			        break;
 			}
 		}
 
 		private static void WaveClear()
 		{
-			
-		}
+		    /*if (waveClearSS[SpellSlot.W].CurrentValue && Spells[SpellSlot.W].IsReady()
+		        && (Player.Instance.ManaPercent >= manaFarm.CurrentValue))
+		    {
+                var creep = ObjectManager.Get<Obj_AI_Minion>().FirstOrDefault(c => c.IsEnemy && c.Health <= Player.Instance.GetAutoAttackDamage(c));
+                if (creep == null) return;
+                Spells[SpellSlot.W].Cast(creep);
+            }*/
+        }
             
 		private static void Combo()
 		{
             var target = TargetSelector.GetTarget(1200f, DamageType.Physical);
-            var rtarget = TargetSelector.GetTarget(3000f, DamageType.Physical);
-            var useW = (comboSpellStatus[SpellSlot.W].CurrentValue && spells[SpellSlot.W].IsReady() && (Player.Instance.ManaPercent >= manaW.CurrentValue));
-		    var useR = (comboSpellStatus[SpellSlot.R].CurrentValue && spells[SpellSlot.R].IsReady() && (Player.Instance.ManaPercent >= manaR.CurrentValue) && rtarget.Distance(Player.Instance) > Player.Instance.GetAutoAttackRange(rtarget));
+            var rtarget = TargetSelector.GetTarget(3000f, DamageType.Magical);
+            var useW = (comboSS[SpellSlot.W].CurrentValue && Spells[SpellSlot.W].IsReady() && (Player.Instance.ManaPercent >= manaW.CurrentValue));
+		    var useR = (comboSS[SpellSlot.R].CurrentValue && Spells[SpellSlot.R].IsReady() && (Player.Instance.ManaPercent >= manaR.CurrentValue) && rtarget.Distance(Player.Instance) > Player.Instance.GetAutoAttackRange(rtarget));
             if (useW)
-				spells[SpellSlot.W].Cast(target);
+				Spells[SpellSlot.W].Cast(target);
 		    if (useR)
-                spells[SpellSlot.R].Cast(rtarget);
+                Spells[SpellSlot.R].Cast(rtarget);
 
+        }
+
+        private static void Harass()
+        {
+            var target = TargetSelector.GetTarget(1200f, DamageType.Physical);
+            if(target == null || Orbwalker.IsAutoAttacking) return;
+            if(harassSS[SpellSlot.W].CurrentValue && Spells[SpellSlot.W].IsReady() && (Player.Instance.ManaPercent >= manaHarass.CurrentValue))
+                Spells[SpellSlot.W].Cast(target);
         }
 
 		private static void SetupSpellStatus()
 		{
-			comboSpellStatus.Add(SpellSlot.Q, null);
-			comboSpellStatus.Add(SpellSlot.W, null);
-			comboSpellStatus.Add(SpellSlot.R, null);
-			waveClearSpellStatus.Add(SpellSlot.Q, null);
-			waveClearSpellStatus.Add(SpellSlot.W, null);
+			comboSS.Add(SpellSlot.Q, null);
+			comboSS.Add(SpellSlot.W, null);
+			comboSS.Add(SpellSlot.R, null);
+			harassSS.Add(SpellSlot.W, null);
+			waveClearSS.Add(SpellSlot.W, null);
 
 		}
 
 		private static void Orbwalker_OnPreAttack(AttackableUnit target, Orbwalker.PreAttackArgs args)
 		{
-            bool useQ = (comboSpellStatus[SpellSlot.Q].CurrentValue && spells[SpellSlot.Q].IsReady() && (Player.Instance.ManaPercent >= manaQ.CurrentValue));
+            bool useQ = (comboSS[SpellSlot.Q].CurrentValue && Spells[SpellSlot.Q].IsReady() && (Player.Instance.ManaPercent >= manaQ.CurrentValue));
 		    if (useQ)
 		    {
 		        if (target.Type == GameObjectType.AIHeroClient)
@@ -108,7 +130,7 @@ namespace dAshe
 		            {
 		                if (buff.Name == "asheqcastready" && buff.Count == 5)
 		                {
-		                    spells[SpellSlot.Q].Cast();
+		                    Spells[SpellSlot.Q].Cast();
 		                }
 		            }
 		        }
